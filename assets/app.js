930acd3aa1839f0c012f2bce6d5ff3959e5ef295
fabien-design <fabienrozier60@gmail.com>
2024-01-17 16:41:57 +0100

    c'est bon full parallax

--- assets/app.js
+++ assets/app.js
@@ -13,9 +13,11 @@ var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
 
 
+
 const cards = document.querySelectorAll(".card");
 let bounds;
-function rotateToMouse(e, card) {
+let img;
+function rotateToMouse(e, card, img) {
   const mouseX = e.clientX;
   const mouseY = e.clientY;
   const leftX = mouseX - bounds.x;
@@ -34,20 +36,24 @@ function rotateToMouse(e, card) {
       ${Math.log(distance) * 3}deg
     )
   `;
+  const depthX = `${50 - (mouseX - bounds.width) * 0.2}%`;
+  const depthY = `${50 - (mouseY - bounds.height) * 0.2}%`;
+  console.log(`${depthX} ${depthY}`);
+  card.style.backgroundPosition = `${depthX} ${depthY}`;
 }
-
 
 cards.forEach((card) => {
   card.addEventListener("mouseenter", (event) => {
     bounds = card.getBoundingClientRect();
+    img = card.querySelector("img");
     card.addEventListener("mousemove", (event) => {
-      rotateToMouse(event, card);
+      rotateToMouse(event, card, img);
     });
   });
 
   card.addEventListener("mouseleave", () => {
-    document.removeEventListener("mousemove", rotateToMouse);
-    card.style.transition = "transform 1s cubic-bezier(0.445, 0.05, 0.55, 0.95)"; // Ajoute la transition CSS
+    card.removeEventListener("mousemove", rotateToMouse);
+    card.style.transition = "transform 1s cubic-bezier(0.23, 1, 0.32, 1)"; // Ajoute la transition CSS
 
     // Revert à la position de base après la transition
     card.style.transform = "scale3d(1, 1, 1) rotate3d(0, 0, 0, 0deg)";
@@ -55,7 +61,7 @@ cards.forEach((card) => {
     // Supprime la transition après qu'elle soit terminée
     setTimeout(() => {
       card.style.transition = "";
-    }, 1000);
+    }, 500);
   });
 });
 
