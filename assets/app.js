4261d12b967df22ab1636d9cc7f7e3cd1568b990
TWhiteShadow <thewhiteshadowgaming@gmail.com>
2024-01-19 12:00:58 +0100

    Work card parallax functioning

--- assets/app.js
+++ assets/app.js
@@ -14,57 +14,22 @@ var parallaxInstance = new Parallax(scene);
 
 
 
-const cards = document.querySelectorAll(".card");
-let bounds;
-let img;
-function rotateToMouse(e, card, img) {
-    const mouseX = e.clientX;
-    const mouseY = e.clientY;
-    const leftX = mouseX - bounds.x;
-    const topY = mouseY - bounds.y;
-    const center = {
-        x: leftX - bounds.width / 2,
-        y: topY - bounds.height / 2,
-    };
-    const distance = Math.sqrt(center.x ** 2 + center.y ** 2);
+// import Atropos library
+import Atropos from 'atropos';
 
-    card.style.transform = `
-    rotate3d(
-      ${center.y / 100},
-      ${-center.x / 100},
-      0,
-      ${Math.log(distance) * 3}deg
-    )
-  `;
-    const depthX = `${(mouseX - bounds.width / 2) * 0.2}%`;
-    const depthY = `${(mouseY - bounds.height / 2) * 0.1}%`;
-    // console.log(`${depthX} ${depthY}`);
-    card.style.backgroundPosition = `${depthX} ${depthY}`;
-}
-
-cards.forEach((card) => {
-    card.addEventListener("mouseenter", (event) => {
-        bounds = card.getBoundingClientRect();
-        img = card.querySelector("img");
-        card.addEventListener("mousemove", (event) => {
-            rotateToMouse(event, card, img);
-        });
-    });
-
-    card.addEventListener("mouseleave", () => {
-        card.removeEventListener("mousemove", rotateToMouse);
-        card.style.transition = "transform 1s cubic-bezier(0.23, 1, 0.32, 1)"; // Ajoute la transition CSS
-
-        // Revert à la position de base après la transition
-        card.style.transform = "scale3d(1, 1, 1) rotate3d(0, 0, 0, 0deg)";
-
-        // Supprime la transition après qu'elle soit terminée
-        setTimeout(() => {
-            card.style.transition = "";
-        }, 500);
+// Initialize
+document.querySelectorAll('.atropos-works').forEach((element) => {
+    Atropos({
+    el: element,
+    activeOffset: 40,
+    duration: 800,
+    // shadow
+    shadow: true,
+    shadowScale: 1,
+    shadowOffset: 80,
+    // rest of parameters
     });
 });
-
 
 
 
