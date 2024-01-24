2e59683037036dec8b99c453123616a5e96df030
TWhiteShadow <thewhiteshadowgaming@gmail.com>
2024-01-24 16:27:12 +0100

    mobile responsive rework

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,35 @@
 $(document).ready(function () {
-	$('#fullpage').fullpage({
-        licenceKey: 'YWx2YXJvdHJpZ28uY29tX01mU2MyTnliMnhzU0c5eWFYcHZiblJoYkd4NVNRcg==',
-		menu: "#sidebar",
-		anchors: ['home', "about", "works", "albums","contacts"],
-		scrollingSpeed: 1000,
-		dragAndMove: true,
-		loopTop: false,
-		loopBottom: true,
-        fitToSection: true,
-        fitToSectionDelay: 600,
+	var isPhoneDevice = "ontouchstart" in document.documentElement; 
+	if(isPhoneDevice){
+		//mobile
+	}
+		else{
+		$('#fullpage').fullpage({
+			responsiveWidth: 768,
+			licenceKey: 'YWx2YXJvdHJpZ28uY29tX01mU2MyTnliMnhzU0c5eWFYcHZiblJoYkd4NVNRcg==',
+			menu: "#sidebar",
+			anchors: ['home', "about", "works", "albums", "contacts"],
+			scrollingSpeed: 1000,
+			dragAndMove: true,
+			loopTop: false,
+			loopBottom: true,
+			fitToSection: true,
+			fitToSectionDelay: 600,
+			afterResponsive: function(isResponsive) {
+				if (isResponsive) {
+					console.log("I'm in responsive mode");
+				}
+			}
+		});
+	}
+
+	var swiper = new Swiper(".mySwiper", {
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+		edgeSwipeDetection: true,
+		loop: true,
+
 	});
 });
