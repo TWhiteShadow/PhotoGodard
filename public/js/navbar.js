bf41ee3d28a45060fbd293ddc308c73d152b7baf
fabien-design <fabienrozier60@gmail.com>
2024-02-26 14:14:23 +0100

    TEST Navbar effect

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,13 +1,42 @@
 var prevScrollpos = window.scrollY;
+let isScrollingToTop = false;
 window.onscroll = function() {
     var currentScrollPos = window.scrollY;
     if (prevScrollpos > currentScrollPos) {
-      document.getElementById("navbar").style.setProperty('top', '0px', 'important');
+        isScrollingToTop = true;
+        document.getElementById("navbar").style.setProperty('top', '0px', 'important');
     } else {
-      document.getElementById("navbar").style.setProperty('top', '-100px', 'important');
+        document.getElementById("navbar").style.setProperty('top', '-100px', 'important');
+        Array.from(document.querySelector(".imagePreview").children).forEach((child) => {
+            child.classList.add("transition-all", "duration-300", "opacity-0");
+        });
     }
     prevScrollpos = currentScrollPos;
 } 
+// Options pour l'observer
+const options = {
+    threshold: 0.2 // seuil de visibilité
+};
+
+// Fonction de callback pour l'observer
+const callback = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting && isScrollingToTop) {
+            // Si l'élément est visible, ajoutez l'opacité aux enfants de .imagePreview
+            Array.from(document.querySelector(".imagePreview").children).forEach((child) => {
+                child.classList.add("opacity-1");
+                child.classList.remove("opacity-0");
+            });
+        } 
+    });
+};
+
+// Créer un nouvel observer avec la fonction de callback et les options
+const observer = new IntersectionObserver(callback, options);
+
+// Observer l'élément .imagePreview
+observer.observe(document.querySelector('.imagePreview'));
+
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     console.log(anchor)
