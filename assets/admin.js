e143f56008fef00dc955853d0a3b7d606758761a
TWhiteShadow <thewhiteshadowgaming@gmail.com>
2024-02-27 17:31:43 +0100

    entrypoint for admin + removed some scripts into components

---
+++ assets/admin.js
@@ -0,0 +1,6 @@
+// any CSS you import will output into a single css file (app.css in this case)
+import "./styles/admin.scss";
+
+// Import Components
+import './js/Spinner.js';
+import "./js/Accordion";
--- assets/app.js
+++ assets/app.js
@@ -2,9 +2,12 @@
 import "./styles/app.scss";
 
 // Import Components
+import './js/Spinner.js';
 import "./js/MenuScrollTo";
 import "./js/MenuBurger";
 import "./js/SidebarObserver";
 import "./js/Atropos.js";
 import "./js/Footer.js";
 import "./js/Parallax.js";
+import "./js/Navbar.js";
+import './js/Login.js';
