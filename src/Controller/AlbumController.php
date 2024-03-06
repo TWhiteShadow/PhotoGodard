758e6f0ea4e88b066799a8361c2ff2d288cebb5e
fabien-design <fabienrozier60@gmail.com>
2024-03-06 12:55:43 +0100

    Adapt infinite scroll for Albums

--- src/Controller/AlbumController.php
+++ src/Controller/AlbumController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Album;
 use App\Form\AlbumLoginType;
+use App\Repository\PhotoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,11 +15,17 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class AlbumController extends AbstractController
 {
     #[Route('/album/{id}', name: 'app_album_show')]
-    public function show(Album $album, AuthenticationUtils $authenticationUtils, Request $request, UserPasswordHasherInterface $passwordEncoder): Response
+    public function show(Album $album, AuthenticationUtils $authenticationUtils, Request $request, PhotoRepository $photoRepository): Response
     {
         $session = $request->getSession();
         if ((!empty($session->get('ROLE_ALBUM_ACCESS')) && $session->get('ROLE_ALBUM_ACCESS') == $album->getUniqId().'-ACCESS') || !empty($this->getUser()) && in_array('ROLE_ADMIN', $this->getUser()->getRoles())) {
-            $photos = $album->getPhotos();
+            
+            $limit = 9;
+            $photos = $photoRepository->findBy(
+                ['album' => $album],
+                ['id' => 'ASC'],
+                $limit,
+            );
             if (empty($photos)) {
                 return $this->redirectToRoute('app_home');
             }
@@ -48,10 +55,17 @@ class AlbumController extends AbstractController
             if ($identifiant == $album->getId() && trim($password) == $album->getPassword()) {
                 $session->set('ROLE_ALBUM_ACCESS', $album->getUniqId().'-ACCESS');
 
-                $photos = $album->getPhotos();
+                $limit = 9;
+                $photos = $photoRepository->findBy(
+                    ['album' => $album],
+                    ['id' => 'ASC'],
+                    $limit,
+                );
+                if (empty($photos)) {
+                    return $this->redirectToRoute('app_home');
+                }
 
                 return $this->render('album/show.html.twig', [
-                    'controller_name' => 'AlbumController',
                     'album' => $album,
                     'photos' => $photos,
                     'session' => $session,
@@ -67,6 +81,32 @@ class AlbumController extends AbstractController
             'error' => $error,
             'favoritePhoto' => $favoritePhoto,
             'title' => $album->getName(),
+        ]);
+    }
+
+    #[Route('/album/{id}/pagination/{offset}', name: 'app_album_show_pagination')]
+    public function pagination(Album $album, PhotoRepository $photoRepository, int $offset): Response
+    {
+        $limit = 9;
+        $offset -= 1;
+        if($offset > 0) {
+            $offset = $offset * $limit;
+        }
+        $photos = $photoRepository->findBy(
+            ['album' => $album],
+            ['id' => 'ASC'],
+            $limit,
+            $offset,
+        );
+        if (empty($photos)) {
+            return new Response('', Response::HTTP_NOT_FOUND);
+        }
+       
+        return $this->render('_pagination_show_photo.html.twig', [
+            'entityName' => $album->getName(),
+            'entity' => $album,
+            'photos' => $photos,
+            'isCateg' => false,
         ]);
     }
 }
